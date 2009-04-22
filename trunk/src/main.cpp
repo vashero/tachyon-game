ac18f8bf08d1037709459ddbc2547779fff9919c
bfisher@ualberta.net <bfisher@ualberta.net@b90cb52e-2f47-11de-8816-6191455234fe>
2009-04-22 18:28:41 +0000

    make configuration portable across OS and build target

--- trunk/src/main.cpp
+++ trunk/src/main.cpp
@@ -2,15 +2,19 @@
 #include "gameException.hpp"
 
 #if defined(WIN32)
-
 #include "windows.h"
-INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+    #if defined(_DEBUG)
+        const String CONFIG_FILE = "config/plugins_win_debug.cfg";
+    #else
+        const String CONFIG_FILE = "config/plugins_win_release.cfg";
+    #endif
+    INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
 #else
-
-	int main (int argc, char *argv[]) {
+    const String CONFIG_FILE = "config/plugins_lin.cfg";
+    int main (int argc, char *argv[]) {
 #endif
 		try {
-			Core core("config/plugins.cfg", "config/Ogre.cfg", "ogre.log",
+			Core core(CONFIG_FILE, "config/Ogre.cfg", "ogre.log",
 					"config/resources.cfg", true);
 			core.run("config/ship.cfg");
 		} catch (GameException &e) {
